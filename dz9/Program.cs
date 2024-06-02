b1c76f04c5bb812d026b12daca761d1a239a5eb9
Татьяна Кожевникова <tatanakozevnikova786@gmail.com>
2024-06-03 00:51:15 +0500

    no message

---
+++ dz9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int> { -1, -2, -5, 0 };
+
+            try
+            {
+                double average = CalculateAverageOfPositiveNumbers(num);
+                Console.WriteLine("Среднее арифмитическое положительных чисел: " + average);
+            }
+            catch (NoPositiveNumbersException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+
+        }
+
+        public static double CalculateAverageOfPositiveNumbers(List<int> num) 
+        {
+            var positiveNum = num.Where(n => n > 0);
+
+            if(!positiveNum.Any())
+            {
+                throw new NoPositiveNumbersException();
+            }
+            return positiveNum.Average();
+        }
+    }
+
+    public class NoPositiveNumbersException : Exception
+    {
+        public NoPositiveNumbersException() : base("Нет позитивного числа.")
+        {
+
+        }
+    }
+}
---
+++ dz9/Task2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactsManager manager = new ContactsManager();
+
+            try
+            {
+                manager.AddContact("Alice", "123-456-7890");
+                manager.AddContact("Bob", "098-765-4321");
+
+                manager.AddContact("", "123-456-7890");
+            }
+            catch (InvalidContactException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.WriteLine("Все контакты:");
+            manager.DisplayContacts();
+
+            var contact = manager.FindContact("Alice");
+            if (contact != null)
+            {
+                Console.WriteLine($"\nКонтакт: {contact}");
+            }
+            else
+            {
+                Console.WriteLine("\nНет контакта.");
+            }
+
+            manager.RemoveContact("Bob");
+
+        }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Contact(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+
+        public override string ToString() //для удобства
+        {
+            return $"Name: {Name}, PhonenUmber: {PhoneNumber}";
+        }
+    }
+
+    public class ContactsManager
+    {
+        private List<Contact> contacts;
+
+        public ContactsManager()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(string name, string phoneNumber) //добавили
+        {
+            if(string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                throw new InvalidContactException("Имя и номер не могут быть пустыми.");
+            }
+            contacts.Add(new Contact(name, phoneNumber));
+        }
+
+        public void RemoveContact(string name) //удалили
+        {
+            var contact = contacts.FirstOrDefault(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (contact != null)
+            {
+                contacts.Remove(contact);
+            }
+            else
+            {
+                Console.WriteLine("Такого контакта нет");
+            }
+        }
+
+        public Contact FindContact(string name) //поиск
+        {
+            return contacts.FirstOrDefault(c => c.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void DisplayContacts() //отобразили всех
+        {
+            if(contacts.Count == 0)
+            {
+                Console.WriteLine("Нет контактов");
+            }
+            else
+            {
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine(contact);
+                }
+            }
+        }
+    }
+
+    public class InvalidContactException : Exception
+    {
+        public InvalidContactException() { }
+
+        public InvalidContactException(string message) : base(message) { }
+
+        public InvalidContactException(string message, Exception inner) : base(message, inner) { }
+    }
+}
